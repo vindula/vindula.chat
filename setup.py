7e4b8dd5fe9422da276d9b97104a74793c4249b4
Cesar Augusto <cesaraugusto@liberiun.com>
2012-04-26 15:28:10 -0300

    alteração do arquivo sql

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ version = '1.0'
 setup(name='vindula.chat',
       version=version,
       description="Vindula Chat",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
